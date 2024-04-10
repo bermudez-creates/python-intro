6ede241ed4230b269cc874955f44d3f3121806ff
Steezy <bermudezalejandro45@gmail.com>
2024-04-10 16:32:04 -0400

    Madlibs printing

--- madlib.py
+++ madlib.py
@@ -4,7 +4,7 @@ root = tkinter.Tk()
 root.title("Mad lib")
 root.resizable(False, False)
 
-canvas = tkinter.Canvas(root, bg="black", width= 300, height=300, borderwidth= 0, highlightthickness=0 )
+canvas = tkinter.Canvas(root, bg="black", width= 500, height=500, borderwidth= 0, highlightthickness=0 )
 canvas.pack()
 root.update()
 
@@ -18,6 +18,56 @@ root_x = int((screen_width / 2) - (root_width / 2))
 root_y = int((screen_height / 2) - (root_height / 2))
 #format: "(w)x(h)+(x)+(y)"
 root.geometry(f"{root_width}x{root_height}+{root_x}+{root_y}")
+
+# Defining madlibs
+def madlib1():
+
+    animals= input('Pick an animal : ')
+    profession = input('Pick a job : ')
+    cloth = input('Name the fit : ')
+    things = input('Something random: ')
+    name= input('Choose a name : ')
+    place = input('Pick a place anywhere : ')
+    verb = input('Describe an action in the present : ')
+    food = input('Choose some food fatass : ')
+    print('Yerr ' + food + ', the photographer said as the camera flashed! ' + name + ' and I had gone to ' + place + ' to get our photos taken on my birthday. The first photo we really wanted was a picture of us dressed as ' + animals + ' pretending to be a ' + profession + '. when we saw the second photo, it was exactly what I wanted. We both looked like ' + things + ' wearing ' + cloth + ' and ' + verb + ' --exactly what I had in mind')
+
+
+def madlib2():
+   
+    adjactive = input('enter adjective : ')
+    color = input('enter a color name : ')
+    thing = input('enter a thing name :')
+    place = input('enter a place name : ')
+    person= input('enter a person name : ')
+    adjactive1 = input('enter a adjactive : ')
+    insect= input('enter a insect name : ')
+    food = input('enter a food name : ')
+    verb = input('enter a verb name : ')
+
+    print('Last night I dreamed I was a ' +adjactive+ ' butterfly with ' + color+ ' splocthes that looked like '+thing+ ' .I flew to ' + place+ ' with my bestfriend and '+person+ ' who was a '+adjactive1+ ' ' +insect +' .We ate some ' +food+ ' when we got there and then decided to '+verb+ ' and the dream ended when I said-- lets ' +verb+ '.')
+
+
+def madlib3():
+
+    person = input('enter person name: ')
+    color = input('enter color : ')
+    foods = input('enter food name : ')
+    adjective = input('enter aa adjective name: ')
+    thing = input('enter a thing name : ')
+    place = input('enter place : ')
+    verb = input('enter verb : ')
+    adverb = input('enter adverb : ')
+    food = input('enter food name: ')
+    things = input('enter a thing name : ')
+   
+    print('Today we picked apple from '+person+ "'s Orchard. I had no idea there were so many different varieties of apples. I ate " +color+ ' apples straight off the tree that tested like '+foods+ '. Then there was a '+adjective+ ' apple that looked like a ' + thing + '.When our bag were full, we went on a free hay ride to '+place+ ' and back. It ended at a hay pile where we got to ' +verb+ ' ' +adverb+ '. I can hardly wait to get home and cook with the apples. We are going to make appple '+food+ ' and '+things+' pies!.')  
+
+# Buttons
+def callback(): 
+  B = canvas.create_rectangle(root, text= 'The Photographer', font ='arial 15', command= madlib1, bg = 'ghost white').place(x=60, y=120)
+  W = canvas.create_rectangle(root, text= 'apple and apple', font ='arial 15', command = madlib3 , bg = 'ghost white').place(x=70, y=180)
+  X = canvas.create_rectangle(root, text= 'The Butterfly', font ='arial 15', command = madlib2, bg = 'ghost white').place(x=80, y=240)
 
 
 
